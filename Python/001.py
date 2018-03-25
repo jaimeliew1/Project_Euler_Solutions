8a3a9139aa55cc9952739c0402111dedcd326e39
Dusty Taylor <33415790+jaimeliew1@users.noreply.github.com>
2018-03-25 14:36:33 +0200

    Add files via upload

---
+++ Python/001.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Solution to Project Euler problem 1
+
+Author: Jaime Liew
+https://github.com/jaimeliew1/Project_Euler_Solutions
+"""
+
+
+def run():
+    return 0
+
+
+if __name__ == "__main__":
+	print(run())
+

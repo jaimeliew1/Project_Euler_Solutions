963c9c6d6571cade8f87341f97a6a2cd1af202bb
jaimeliew1 <jaimeliew1@gmail.com>
2021-07-23 15:19:51 +0200

    Update sieve of eratosthenes prime generator.

--- Python/EulerFunctions.py
+++ Python/EulerFunctions.py
@@ -4,15 +4,15 @@ from collections import Counter
 import time
 
 
-
 def timeit(method):
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
         te = time.time()
 
-        print('Time elapsed for {}: {:2.4f}s'.format(method.__name__, (te - ts)))
+        print("Time elapsed for {}: {:2.4f}s".format(method.__name__, (te - ts)))
         return result
+
     return timed
 
 
@@ -21,48 +21,36 @@ def numDigits(n):
     return math.floor(math.log10(n)) + 1
 
 
-# primes_sieve used in problems: ..., 27,
-def primes_sieve(n):
-    # Returns boolean array indicating if each number is prime or not.
-    # Sieve of Eratosthenes.
-    a = [True] * (n+1)
-    a[0] = a[1] = False
+def primes_sieve(N):
+    is_prime = [True] * (N + 1)
+    is_prime[0] = is_prime[1] = False
 
-    for (i, isprime) in enumerate(a):
-        if isprime:
+    for i in trange(2, int(N ** 0.5) + 1):
+        if not is_prime[i]:
+            continue
+        for j in range(i * i, N + 1, i):
+            is_prime[j] = False
+    return is_prime
 
-            for n in range(i*i, len(a), i):     # Mark factors non-prime
-                a[n] = False
-    return a
 
 def primes_gen(n):
     # Generates prime numbers p < n
-    a = [True] * (n+1)
+    a = [True] * (n + 1)
     a[0] = a[1] = False
 
     for (i, isprime) in enumerate(a):
         if isprime:
             yield i
-            for n in range(i*i, len(a), i):     # Mark factors non-prime
+            for n in range(i * i, len(a), i):  # Mark factors non-prime
                 a[n] = False
-
-#def list_primality(n):
-#	# Sieve of Eratosthenes
-#	result = [True] * (n + 1)
-#	result[0] = result[1] = False
-#	for i in range(sqrt(n) + 1):
-#		if result[i]:
-#			for j in range(i * i, len(result), i):
-#				result[j] = False
-#	return result
 
 
 def primeFactors(x, primes, aslist=False):
     primefactors = Counter()
     while x > 1:
         for p in primes:
-            if x%p == 0:
-                x = x//p
+            if x % p == 0:
+                x = x // p
                 primefactors[p] += 1
                 break
     if aslist:
@@ -70,8 +58,9 @@ def primeFactors(x, primes, aslist=False):
     else:
         return dict(primefactors)
 
+
 def factors(x):
-    factors = list([i, x//i] for i in range(1, int(pow(x, 0.5) + 1)) if x % i == 0)
+    factors = list([i, x // i] for i in range(1, int(pow(x, 0.5) + 1)) if x % i == 0)
     return set(functools.reduce(list.__add__, factors))
 
 
@@ -79,12 +68,13 @@ def primelist(n):
     # Returns list of prime numbers below n.
     return [i for i, p in enumerate(primes_sieve(n)) if p]
 
+
 def multiplicative_partitions(n, min_divisor=2):
-    #https://stackoverflow.com/questions/24723721/how-can-i-generate-all-possible-divisor-products-for-a-number
+    # https://stackoverflow.com/questions/24723721/how-can-i-generate-all-possible-divisor-products-for-a-number
     """Generate expressions of n as a product of ints >= min_divisor."""
     if n == 1:
         yield []
-    for divisor in range(min_divisor, n+1):
+    for divisor in range(min_divisor, n + 1):
         if n % divisor == 0:
             for product in multiplicative_partitions(n // divisor, divisor):
                 yield product + [divisor]
@@ -94,12 +84,13 @@ def _try_composite(a, d, n, s):
     if pow(a, d, n) == 1:
         return False
     for i in range(s):
-        if pow(a, 2**i * d, n) == n-1:
+        if pow(a, 2 ** i * d, n) == n - 1:
             return False
-    return True # n  is definitely composite
+    return True  # n  is definitely composite
+
 
 def is_prime(n, _precision_for_huge_n=16):
-    #Miller–Rabin primality test
+    # Miller–Rabin primality test
     if n == 1:
         return False
     if n in _known_primes or n in (0, 1):
@@ -125,8 +116,10 @@ def is_prime(n, _precision_for_huge_n=16):
     if n < 341550071728321:
         return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
     # otherwise
-    return not any(_try_composite(a, d, n, s)
-                   for a in _known_primes[:_precision_for_huge_n])
+    return not any(
+        _try_composite(a, d, n, s) for a in _known_primes[:_precision_for_huge_n]
+    )
+
 
 _known_primes = [2, 3]
 _known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
